4aab621ac30a8231262aa4db239fe8d19b257e95
Ebrahim Mostafa <ibrahim.mostafa9939@gmail.com>
2025-10-05 07:45:02 +0300

    project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {path: '', redirectTo: 'home',title: 'Home', pathMatch: 'full'},
+      {path: 'home', redirectTo: 'home', title: 'Home', pathMatch: 'full'},
+      {path: 'contact', redirectTo: 'contact', title: 'Contact', pathMatch: 'full'},
+      {path: 'project', redirectTo: 'project', title: 'Project', pathMatch: 'full'},
+      {path: 'about', redirectTo: 'about', title: 'About', pathMatch: 'full'},
+      {path: 'contact', redirectTo: 'contact', title: 'Contact', pathMatch: 'full'},
+      {path: 'skills', redirectTo: 'skills', title: 'Skills', pathMatch: 'full'},
+];
